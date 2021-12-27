0b30d4d7670bdd0daaf29a415b0249e90e43f0ce
pcorpx <pcorpx@gmail.com>
2021-12-27 13:22:12 +0500

    generate json with place data

--- places/urls.py
+++ places/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('<int:place_id>/', views.get_place_details)
 ]
--- places/views.py
+++ places/views.py
@@ -1,29 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from django.template import loader
+from django.http.response import JsonResponse
 
-from places.models import Place
+
+from places.models import Place, Image
 
 def index(request):
     features = []
 
     for place in Place.objects.all():
         feature = {
-                      "type": "Feature",
-                      "geometry": {
-                          "type": "Point",
-                          "coordinates": [place.lng, place.lat]
+                      'type': 'Feature',
+                      'geometry': {
+                          'type': 'Point',
+                          'coordinates': [place.lng, place.lat]
                       },
-                      "properties": {
-                           "title": place.title,
-                           "placeId": place.placeId,
-                           "detailsUrl": "/places"
+                      'properties': {
+                           'title': place.title,
+                           'placeId': place.placeId,
+                           'detailsUrl': '/places'
                       }
                   }
         features.append(feature)
 
     places_data = {
-        "type": "FeatureCollection",
-        "features": features
+        'type': 'FeatureCollection',
+        'features': features
     }
     return render(request, 'index.html', context={'places_data': places_data})
+
+def get_place_details(request, place_id):
+    place = get_object_or_404(Place, id=place_id)
+    images = []
+    for image in Image.objects.all():
+        images.append(image.image.url)
+    place_data = {
+        'title': place.title,
+        'imgs': images,
+        'description_short': place.description_short,
+        'description_long': place.description_long,
+        'coordinates': {
+            'lng': place.lng,
+            'lat': place.lat
+        }
+    }
+    return JsonResponse(place_data, json_dumps_params={'ensure_ascii': False, 'indent': 4})
