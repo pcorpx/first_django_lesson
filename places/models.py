6a602f68fd42b341c89913313552a123c6454c68
pcorpx <pcorpx@gmail.com>
2021-12-27 11:34:02 +0500

    change data source to data base

--- places/models.py
+++ places/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Place(models.Model):
     title = models.CharField(max_length=200)
+    placeId = models.CharField(max_length=200)
     description_short = models.TextField()
     description_long = models.TextField(blank=True)
     lat = models.FloatField()
--- places/views.py
+++ places/views.py
@@ -2,35 +2,28 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+from places.models import Place
 
 def index(request):
-    value = {
+    features = []
+
+    for place in Place.objects.all():
+        feature = {
+                      "type": "Feature",
+                      "geometry": {
+                          "type": "Point",
+                          "coordinates": [place.lng, place.lat]
+                      },
+                      "properties": {
+                           "title": place.title,
+                           "placeId": place.placeId,
+                           "detailsUrl": "/places"
+                      }
+                  }
+        features.append(feature)
+
+    places_data = {
         "type": "FeatureCollection",
-        "features": [
-            {
-                "type": "Feature",
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [37.62, 55.793676]
-                },
-                "properties": {
-                "title": "«Легенды Москвы",
-                "placeId": "moscow_legends",
-                "detailsUrl": "/static/places/moscow_legends.json"
-                }
-            },
-            {
-                "type": "Feature",
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [37.64, 55.753676]
-                },
-                "properties": {
-                "title": "Крыши24.рф",
-                "placeId": "roofs24",
-                "detailsUrl": "/static/places/roofs24.json"
-                }
-            }
-        ]
+        "features": features
     }
-    return render(request, 'index.html', context={'value': value})
+    return render(request, 'index.html', context={'places_data': places_data})
