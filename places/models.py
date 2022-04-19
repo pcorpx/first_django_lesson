4a57a87bff748995d035821fc5c84f0300b3a3ab
Полежаев Игорь Олегович <ingvar@kontur>
2022-04-19 11:58:51 +0500

    add verbose name

--- places/models.py
+++ places/models.py
@@ -3,22 +3,29 @@ from tinymce import models as tinymce_models
 
 
 class Place(models.Model):
-    title = models.CharField(max_length=200, unique=True)
-    slug = models.CharField(max_length=200, unique=True)
-    description_short = models.TextField()
-    description_long = tinymce_models.HTMLField()
-    lat = models.FloatField()
-    lng = models.FloatField()
+    title = models.CharField(max_length=200, unique=True,
+                             verbose_name='Название')
+    slug = models.CharField(max_length=200, unique=True,
+                            verbose_name='Метка')
+    description_short = models.TextField(
+        verbose_name='Короткое описание'
+    )
+    description_long = tinymce_models.HTMLField(
+        verbose_name='Полное описание'
+    )
+    lat = models.FloatField(verbose_name='Широта')
+    lng = models.FloatField(verbose_name='Долгота')
 
     def __str__(self):
         return self.title
 
 
 class Image(models.Model):
-    image = models.ImageField(upload_to='images')
-    position = models.IntegerField(default=0, blank=False, null=False)
+    image = models.ImageField(upload_to='images', verbose_name='Изображение')
+    position = models.IntegerField(default=0, blank=False, null=False,
+                                   verbose_name='Позиция')
     place = models.ForeignKey(Place, on_delete=models.CASCADE,
-                              related_name='images')
+                              related_name='images', verbose_name='Место')
 
     class Meta:
         ordering = ['position']
