a44daf5056218897e28b8f9dbf1d757d9d329fe1
Полежаев Игорь Олегович <ingvar@kontur>
2022-04-22 18:29:20 +0500

    add statusc code check

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -1,8 +1,20 @@
+from doctest import Example
+import requests
+import logging
 from django.core.management.base import BaseCommand
 from django.core.files.base import ContentFile
-import requests
 from slugify import slugify
+from json.decoder import JSONDecodeError
+
 from places.models import Place, Image
+
+
+logging.basicConfig(
+    format = u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s \
+             [%(asctime)s] %(message)s',
+    handlers=[logging.FileHandler('mylog.log', 'a', 'utf-8')], 
+    level = logging.DEBUG
+)
 
 
 class Command(BaseCommand):
@@ -13,7 +25,7 @@ class Command(BaseCommand):
 
     def attache_image(self, image_link, requested_image,
                       created_place, position):
-        image = ContentFile(requested_image.content)
+        image = ContentFile(requested_image)
         created_image = Image.objects.create(position=position,
                                              place=created_place)
         image_name = image_link.split('/')[-1]
@@ -24,10 +36,36 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         for src_link in options['src_link']:
             try:
-                requested_place = requests.get(src_link).json()
+                response = requests.get(src_link)
+                response.raise_for_status()
+                requested_place = response.json()
+                if 'error' in requested_place:
+                    raise requests.exceptions.HTTPError(requested_place['error'])
+            except requests.exceptions.HTTPError as err:
+                self.stdout.write(self.style.ERROR('Http Error ' + str(err)))
+                logging.debug(err)
+                continue
+            except requests.exceptions.ConnectionError as err:
+                self.stdout.write(self.style.ERROR('Error Connecting ' + str(err)))
+                logging.debug(err)
+                continue
+            except requests.exceptions.Timeout as err:
+                self.stdout.write(self.style.ERROR('Timeot Error ' + str(err)))
+                logging.debug(err)
+                continue
+            except requests.exceptions.JSONDecodeError:
+                self.stdout.write(self.style.ERROR('Non valid format of json file'))
+                logging.debug('Non valid format of json file')
+                continue
+            except requests.exceptions.MissingSchema:
+                self.stdout.write(self.style.ERROR('Non valid format of url (ex. http://name.ru)'))
+                logging.debug('Non valid format of url. (ex. http://name.ru)')
+                continue
             except requests.exceptions.RequestException as err:
                 self.stdout.write(self.style.ERROR(err))
+                logging.debug(err)
                 continue
+
             created_place, created = Place.objects.get_or_create(
                 slug=slugify(requested_place['title']),
                 defaults={
@@ -42,9 +80,28 @@ class Command(BaseCommand):
                 position = 0
                 for image_link in requested_place['imgs']:
                     try:
-                        requested_image = requests.get(image_link)
+                        response = requests.get(image_link)
+                        response.raise_for_status()
+                        requested_image = response.content
+                    except requests.exceptions.HTTPError as err:
+                        self.stdout.write(self.style.ERROR('Http Error ' + str(err)))
+                        logging.debug(err)
+                        continue
+                    except requests.exceptions.ConnectionError as err:
+                        self.stdout.write(self.style.ERROR('Error Connecting ' + str(err)))
+                        logging.debug(err)
+                        continue
+                    except requests.exceptions.Timeout as err:
+                        self.stdout.write(self.style.ERROR('Timeot Error ' + str(err)))
+                        logging.debug(err)
+                        continue
+                    except requests.exceptions.MissingSchema:
+                        self.stdout.write(self.style.ERROR('Non valid format of url (ex. http://name.ru)'))
+                        logging.debug('Non valid format of url. (ex. http://name.ru)')
+                        continue
                     except requests.exceptions.RequestException as err:
                         self.stdout.write(self.style.ERROR(err))
+                        logging.debug(err)
                         continue
                     try:
                         self.attache_image(image_link, requested_image,
