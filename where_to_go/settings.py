cf5ba074211d0f73521a14002d21615032df834d
Полежаев Игорь Олегович <ingvar@kontur>
2022-04-22 14:43:34 +0500

    change default setting for ALLOWED_HOSTS

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env.str('SECRET_KEY', 'default_secret')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', False)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', '*')
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', '127.0.0.1')
 
 # Application definition
 
